b44fcc178faa101bbe432bb84cb27d4cde171b8e
nungerleider <n.ungerleider@gmail.com>
2018-06-11 14:01:39 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='circVis',
+    version='0.1dev',
+    packages=['circvis',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.txt').read(),
+)
