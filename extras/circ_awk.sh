691c0e90656300aeecfd8d0435f06de4ee4562a9
nungerleider <n.ungerleider@gmail.com>
2018-06-14 11:45:48 -0500

    fixed star convert

--- extras/circ_awk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-## Converts find_circ output file to compatible bed format
-
-awk 'BEGIN {OFS="\t"} { if ($4 ~ /circ/) print $1,$2,$3,$6,$7,$0 }' $1 > ${1}.build_db.bed
--- extras/sj_awk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-## Converts output STAR splice junction file to compatible canonical bed file
-
-awk ' BEGIN { OFS="\t" } {print $1,$2,$3,$6,$5 }' $1 |sed '1d'>${1}.splice_plot_ready.canonical.bed
-
